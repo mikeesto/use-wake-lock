0db5c769dd64422c961b294c6e29f5d7927c5333
Michael Esteban <mickel13@gmail.com>
2020-07-21 14:38:18 +1000

    Updates

---
+++ precache-manifest.dab854581eacf4fe16fa33e1a76e9f43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6316cf4d7d906a9429e3d668fe20a7b1",
+    "url": "/use-wake-lock/index.html"
+  },
+  {
+    "revision": "4c3be09d47a9087bb76d",
+    "url": "/use-wake-lock/static/css/main.d03926d9.chunk.css"
+  },
+  {
+    "revision": "8ed23b1f96335fe0ae94",
+    "url": "/use-wake-lock/static/js/2.7fdb427a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/use-wake-lock/static/js/2.7fdb427a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3be09d47a9087bb76d",
+    "url": "/use-wake-lock/static/js/main.36415920.chunk.js"
+  },
+  {
+    "revision": "001fc7fb2b77506f611e",
+    "url": "/use-wake-lock/static/js/runtime-main.59103925.js"
+  }
+]);
